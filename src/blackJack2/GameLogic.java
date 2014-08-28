c88b1df0ba7dd59c2f0bf81626e58953b012589a
Dimitar Bochev <fgg@mail.bg>
2014-08-28 16:25:59 +0300

    begin

---
+++ src/blackJack2/GameLogic.java
@@ -0,0 +1,81 @@
+package blackJack2;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class GameLogic {
+	
+	static int pointsAll = 0;
+	static ArrayList<Integer> pastCards = new ArrayList<Integer>();
+	
+	public static void random(){
+		Random rand = new Random();
+		int cardDrawn=rand.nextInt(44)+5;
+		if(cardCheck(cardDrawn)){
+			random();
+		}
+		cardDefine(cardDrawn);
+		
+	}
+	private static boolean cardCheck(int CardDrown){
+		boolean isExist=false;
+		if(pastCards.contains(CardDrown)){
+			isExist=true;
+			return isExist;
+		} else{
+			isExist=false;
+			return isExist;	
+		}
+	}
+	
+	public static void cardDefine(int cardDrown) {
+		int points, collor;
+		
+		String cardSuit = new String();
+		if(cardDrown%4==0){
+			points=cardDrown/4+1;
+		}else{
+			points=cardDrown/4+2;
+		}if(points<=10){
+			cardSuit= ""+points;
+		}switch (points) {
+		case 11:
+			cardSuit="J";
+			points =10;
+			break;
+		case 12:
+			cardSuit="Q";
+			points =10;
+			break;
+		case 13:
+			cardSuit="K";
+			points =10;
+			break;
+		case 14:
+			cardSuit="A";
+			points =11;
+			break;
+		}
+		
+		collor= cardDrown%4;
+		switch (collor) {
+		case 0:
+			cardSuit+="♣";
+			break;
+		case 1:
+			cardSuit+="♠";
+			break;
+		case 2:
+			cardSuit+="♥";
+			break;
+		case 3:
+			cardSuit+="♦";
+			break;
+		}
+		pointsAll+=points;
+		//System.out.println(cardSuit+" "+points);
+		//pointsChecker();
+		System.out.println("padna ti se: "+cardSuit+" ima6 to4ki "+pointsAll);
+	}
+
+}
---
+++ src/blackJack2/Main.java
@@ -0,0 +1,10 @@
+package blackJack2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		GameLogic.random();
+
+	}
+
+}
